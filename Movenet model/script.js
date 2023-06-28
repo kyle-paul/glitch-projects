eedcd4820f2666081c8589a0cc94820ed084bda3
Paul Delta <117391498+DeltaPaul4@users.noreply.github.com>
2023-06-29 00:49:17 +0700

    movenet

---
+++ Movenet model/script.js
@@ -0,0 +1,39 @@
+// notification when the model is loaded
+const status = document.getElementById('status');
+if (status) {
+  status.innerText = 'Loaded TensorFlow.js - version: ' + tf.version.tfjs;
+}
+
+const MODEL_PATH = 'https://tfhub.dev/google/tfjs-model/movenet/singlepose/lightning/4';
+const EXAMPLE_IMG = document.getElementById('exampleImg');
+let movenet = undefined
+
+async function loadAndRunModel() {
+  movenet = await tf.loadGraphModel(MODEL_PATH, { fromTFHub: true });
+  // let exampleInputTensor = tf.zeros([1, 192, 192, 3], 'int32');
+  let imageTensor = tf.browser.fromPixels(EXAMPLE_IMG)
+  console.log(imageTensor.shape);
+  
+  // crop the image
+  let cropStartPoint = [15, 170, 0];
+  let cropSize = [345, 345, 3];
+  let croppedTensor = tf.slice(imageTensor, cropStartPoint, cropSize);
+  
+  // resize the image
+  let resizedTensor = tf.image.resizeBilinear(croppedTensor, [192, 192], true).toInt();
+  console.log(resizedTensor.shape);
+  
+  // model prediction
+  let tensorOutput = movenet.predict(tf.expandDims(resizedTensor));
+  let arrayOutput = await tensorOutput.array();
+  console.log(arrayOutput);
+  
+  // depose to avoid memory leak
+  imageTensor.dispose();
+  croppedTensor.dispose();
+  resizedTensor.dispose();
+  tensorOutput.dispose();
+}
+
+
+loadAndRunModel();
